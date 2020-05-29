d1895c36efb410dba5cfe44eaa4312947fc39509
Suffren <regis.cleis@hotmail.fr>
2020-05-29 12:25:15 +0200

    api.py: ensure it is a main module & handle text data type

--- api/api.py
+++ api/api.py
@@ -1,17 +1,20 @@
 # -*- coding: utf-8 -*-
 
 import sqlite3
-conn = sqlite3.connect('reports.db')
 
-cur = conn.cursor()
-cur.execute('CREATE TABLE reports (label text)')
-cur.execute("INSERT INTO reports VALUES('Ampoule defecteuse')")
+def main():
+    conn = sqlite3.connect('reports.db')
+    # Controls what objects are returned for the 'text' data type
+    conn.text_factory = lambda x: x.decode('latin-1')
+    cur = conn.cursor()
+    cur.execute('CREATE TABLE reports (label text)')
+    cur.execute("INSERT INTO reports VALUES('Ampoule defecteuse')")
+    # Save
+    conn.commit()
+    cur.execute('SELECT * FROM reports')
+    print cur.fetchall()
+    conn.close()
 
-# Save
-conn.commit()
-
-cur.execute('SELECT * FROM reports')
-print cur.fetchall()
-
-
-conn.close()
+# Ensure this module has not been imported
+if __name__ == '__main__':
+    main()
