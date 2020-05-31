c1a19df08efe5d3ccada9a9eab53cd2a614f234e
Suffren <regis.cleis@hotmail.fr>
2020-05-31 12:15:32 +0200

    app.js: create 'post' method

--- app.js
+++ app.js
@@ -37,4 +37,34 @@ angular.module('app', [])
             console.error('Angular error: ', response);
         });
     }
-});
+
+    this.post = function(URI, data) {
+        return $http({
+            method: 'POST',
+            url: URL + URI,
+            headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+            /*  Converts an object to URL parameter(s)
+                This function only works with objects */
+            transformRequest: function(obj) {
+                return Object.keys(obj).map(function(d) {
+                    return encodeURIComponent(d) + '=' + encodeURIComponent(obj[d]);
+                }).join('&');
+            },
+            data: data
+        }).then(function successCallback(response) {
+            if(response.status === 200) {
+                return response.data;
+            } else {
+                throw new Error("Une erreur est survenue.");
+            }
+        }, function errorCallback(response) {
+            // TODO: Redirect to 404 ?
+            if (response.status) {
+               console.warn( response.data.error || "Server error");
+               return;
+            }
+            // TODO: send the error to the devs
+            console.error('Angular error: ', response);
+        });
+    }
+})
