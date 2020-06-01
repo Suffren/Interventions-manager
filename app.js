802548d230b23b18610f96a7c482802933450b37
Suffren <regis.cleis@hotmail.fr>
2020-06-01 12:33:46 +0200

    app.js, index: can now update a report using Modal service ModalIntervention: remove bindings cause they're not used anymore

--- app.js
+++ app.js
@@ -51,14 +51,42 @@ angular.module('app', [])
     templateUrl: 'modal.html',
     controller: function (API, Modal) {
         var ctrl = this;
+        var report = Modal.data;
+
+        // Fill the inputs
+        if(report) {
+            API.get('/report' + '?id=' + Modal.data.report_id).then(
+                function(res) {
+                    ctrl.form = res;
+                },
+                function(error) {
+                    alert(error);
+                }
+            );
+        }
 
         ctrl.confirm = function() {
-            ctrl.create(ctrl.form);
+            if(report) {
+                ctrl.update(ctrl.form, Modal.data.report_id);
+            } else {
+                ctrl.create(ctrl.form);
+            }
         }
 
         ctrl.create = function(form) {
             API.post('/add_report', form).then(
                 function(res) {
+                    Modal.close();
+                },
+                function(error) {
+                    alert(error);
+                }
+            );
+        }
+
+        ctrl.update = function(data, report_id) {
+            API.post('/update_report' + '?id=' + report_id, data).then(
+                function() {
                     Modal.close();
                 },
                 function(error) {
